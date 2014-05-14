4155597a1de7985785574bfac12b73f8f02f3839
Matt Neary <neary.matt@gmail.com>
2014-05-14 17:16:04 -0400

    semantic equality on expressions

--- redux.hs
+++ redux.hs
@@ -5,11 +5,15 @@ instance Show Expr where
   show (Var x) = x
   show (Abstract x y) = "λ" ++ x ++ "." ++ (show y)
   show (Apply x y) = "(" ++ (show x) ++ ")" ++ (show y)
+instance Eq Expr where
+  (Var x) == (Var y) = x == y
+  (Abstract a x) == (Abstract b y) = x == (subst y b a)
+  (Apply a b) == (Apply x y) = x == y
 
 subst :: Expr -> String -> String -> Expr
 subst (Var x) a b = if x == a then Var b else Var x
 subst (Abstract x y) a b = if x == a then (Abstract x y) else (Abstract x (subst y a b))
 subst (Apply x y) a b = Apply (subst x a b) (subst y a b)
 
-main = putStrLn . show $ Apply (Abstract "x" (Var "x")) (Abstract "x" (Var "x"))
+main = putStrLn . show $ (Abstract "x" (Var "x")) == (Abstract "y" (Var "y"))
 
