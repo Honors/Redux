9a97d989ffbb3d98962ca6d9c207c414d8aac4c6
Matt Neary <neary.matt@gmail.com>
2014-05-14 17:23:39 -0400

    substitution made to work on/with any expression type

--- redux.hs
+++ redux.hs
@@ -7,12 +7,14 @@ instance Show Expr where
   show (Apply x y) = "(" ++ (show x) ++ ")" ++ (show y)
 instance Eq Expr where
   (Var x) == (Var y) = x == y
-  (Abstract a x) == (Abstract b y) = x == (subst y b a)
+  (Abstract a x) == (Abstract b y) = x == (subst y (Var b) (Var a))
   (Apply a b) == (Apply x y) = x == y
 
-subst :: Expr -> String -> String -> Expr
-subst (Var x) a b = if x == a then Var b else Var x
-subst (Abstract x y) a b = if x == a then (Abstract x y) else (Abstract x (subst y a b))
+subst :: Expr -> Expr -> Expr -> Expr
+subst (Var x) (Var a) b = if x == a then b else Var x
+subst (Var x) a b = Var x
+subst (Abstract x y) (Var a) b = if x == a then (Abstract x y) else (Abstract x (subst y (Var a) b))
+subst (Abstract x y) a b = Abstract x (subst y a b)
 subst (Apply x y) a b = Apply (subst x a b) (subst y a b)
 
 main = putStrLn . show $ (Abstract "x" (Var "x")) == (Abstract "y" (Var "y"))
