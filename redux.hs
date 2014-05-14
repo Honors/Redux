ea00f1989c1fd7e16fd76241c61d005fb26e8abc
Matt Neary <neary.matt@gmail.com>
2014-05-14 18:04:55 -0400

    halting reduction on fixed points

--- redux.hs
+++ redux.hs
@@ -9,6 +9,7 @@ instance Eq Expr where
   (Var x) == (Var y) = x == y
   (Abstract a x) == (Abstract b y) = x == (subst y (Var b) (Var a))
   (Apply a b) == (Apply x y) = x == y
+  x == y = False
 
 subst :: Expr -> Expr -> Expr -> Expr
 subst (Var x) (Var a) b = if x == a then b else Var x
@@ -18,10 +19,14 @@ subst (Abstract x y) a b = Abstract x (subst y a b)
 subst (Apply x y) a b = Apply (subst x a b) (subst y a b)
 
 reduce :: Expr -> Expr
-reduce (Apply (Abstract x y) a) = subst y (Var x) a 
-reduce (Apply f x) = Apply (reduce f) (reduce x)
+reduce (Apply (Abstract x y) a) =
+  let subbed = subst y (Var x) a 
+  in if subbed == (Apply (Abstract x y) a) then subbed else reduce subbed
 reduce (Abstract x y) = Abstract x (reduce y)
 reduce x = x
 
-main = putStrLn . show . reduce $ Apply (Abstract "x" (Var "x")) (Abstract "x" (Var "x"))
+main = putStrLn . show . reduce $
+  Apply
+    (Abstract "f" (Apply (Var "f") (Var "f")))
+    (Abstract "f" (Apply (Var "f") (Var "f")))
 
